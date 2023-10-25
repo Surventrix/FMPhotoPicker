be2185f7b816a8da73be3482df8a9fc1f24de0a4
jayeshtejwani8055 <mobiledeveloper.jayesh@gmail.com>
2023-10-25 17:29:13 +0530

    Orientation bug fix

--- FMPhotoPicker/FMPhotoPicker/source/Data/FMPhotoAsset.swift
+++ FMPhotoPicker/FMPhotoPicker/source/Data/FMPhotoAsset.swift
@@ -12,21 +12,21 @@ import Photos
 public class FMPhotoAsset {
     let asset: PHAsset?
     let sourceImage: UIImage?
-    
+
     var mediaType: FMMediaType
-    
+
     // a fully edited thumbnail version of the image
     var editedThumb: UIImage?
-    
+
     // a filterd-only thumbnail version of the image
     var filterdThumb: UIImage?
-    
+
     var thumbRequestId: PHImageRequestID?
-    
+
     var videoFrames: [CGImage]?
-    
+
     var thumbChanged: (UIImage) -> Void = { _ in }
-    
+
     private var fullSizePhotoRequestId: PHImageRequestID?
     private var editor: FMImageEditor!
     /**
@@ -35,28 +35,28 @@ public class FMPhotoAsset {
      https://stackoverflow.com/questions/48657304/phimagemanagers-cancelimagerequest-does-not-work-as-expected?noredirect=1#comment84332723_48657304
      */
     private var canceledFullSizeRequest = false
-    
+
     init(asset: PHAsset, forceCropType: FMCroppable?) {
         self.asset = asset
         self.mediaType = FMMediaType(withPHAssetMediaType: asset.mediaType)
         self.sourceImage = nil
-        
         self.editor = self.initializeEditor(for: forceCropType, imageSize: CGSize(width: asset.pixelWidth, height: asset.pixelHeight))
+        self.requestThumb { _ in }
     }
-    
+
     init(sourceImage: UIImage, forceCropType: FMCroppable?) {
         self.sourceImage = sourceImage
         self.mediaType = .image
         self.asset = nil
-        
         self.editor = self.initializeEditor(for: forceCropType, imageSize: sourceImage.size)
+        self.requestThumb { _ in }
     }
-    
+
     private func initializeEditor(for forceCropType: FMCroppable?, imageSize: CGSize) -> FMImageEditor {
         guard let forceCropType = forceCropType, let fmCropRatio = forceCropType.ratio() else {
             return FMImageEditor()
         }
-        
+
         let imageRatio = CGFloat(imageSize.width) / CGFloat(imageSize.height)
         let cropRatio = fmCropRatio.width / fmCropRatio.height
         var scaleW, scaleH: CGFloat
@@ -73,7 +73,7 @@ public class FMPhotoAsset {
                                   scaleH: scaleH)
         return FMImageEditor(filter: kDefaultFilter, crop: forceCropType, cropArea: cropArea)
     }
-    
+
     func requestVideoFrames(_ complete: @escaping ([CGImage]) -> Void) {
         if let videoFrames = self.videoFrames {
             complete(videoFrames)
@@ -88,7 +88,7 @@ public class FMPhotoAsset {
             }
         }
     }
-    
+
     func requestThumb(refresh: Bool=false, _ complete: @escaping (UIImage?) -> Void) {
         if let editedThumb = self.editedThumb, !refresh {
             complete(editedThumb)
@@ -100,12 +100,12 @@ public class FMPhotoAsset {
             if let asset = asset {
                 self.thumbRequestId = Helper.getPhoto(by: asset, in: size) { image in
                     self.thumbRequestId = nil
-                    
+
                     guard let image = image else { return complete(nil) }
-                    
+
                     self.editedThumb    = self.editor.reproduce(source: image, cropState: .edited, filterState: .edited)
                     self.filterdThumb   = self.editor.reproduce(source: image, cropState: .edited, filterState: .original)
-                    
+
                     complete(self.editedThumb)
                 }
             } else {
@@ -115,7 +115,7 @@ public class FMPhotoAsset {
             }
         }
     }
-    
+
     func requestImage(in desireSize: CGSize, _ complete: @escaping (UIImage?) -> Void) {
         if let asset = asset {
             _ = Helper.getPhoto(by: asset, in: desireSize) { image in
@@ -129,7 +129,7 @@ public class FMPhotoAsset {
             complete(edited)
         }
     }
-    
+
     func requestFullSizePhoto(cropState: FMImageEditState, filterState: FMImageEditState, complete: @escaping (UIImage?) -> Void) {
         if let asset = asset {
             self.fullSizePhotoRequestId = Helper.getPhoto(by: asset, in: CGSize(width: 2000, height: 2000)){ image in
@@ -149,49 +149,49 @@ public class FMPhotoAsset {
             complete(result)
         }
     }
-    
+
     public func cancelAllRequest() {
         self.cancelThumbRequest()
         self.cancelFullSizePhotoRequest()
     }
-    
+
     public func cancelThumbRequest() {
         if let thumbRequestId = self.thumbRequestId {
             PHImageManager.default().cancelImageRequest(thumbRequestId)
         }
     }
-    
+
     public func cancelFullSizePhotoRequest() {
         if let fullSizePhotoRequestId = self.fullSizePhotoRequestId {
             PHImageManager.default().cancelImageRequest(fullSizePhotoRequestId)
             self.canceledFullSizeRequest = true
         }
     }
-    
+
     public func getAppliedFilter() -> FMFilterable {
         return editor.filter
     }
-    
+
     public func getAppliedCrop() -> FMCroppable {
         return editor.crop
     }
-    
+
     public func getAppliedCropArea() -> FMCropArea {
         return editor.cropArea
     }
-    
+
     public func apply(filter: FMFilterable, crop: FMCroppable, cropArea: FMCropArea) {
         editor.filter = filter
         editor.crop = crop
         editor.cropArea = cropArea
-        
+
         requestThumb(refresh: true) { image in
             if let image = image {
                 self.thumbChanged(image)
             }
         }
     }
-    
+
     public func isEdited() -> Bool {
         return editor.isEdited()
     }
