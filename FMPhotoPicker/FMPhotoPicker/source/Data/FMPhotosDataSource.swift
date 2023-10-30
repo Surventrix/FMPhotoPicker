9ffa5de910e35b7ebfa30b42fd9ba34d5cececf6
jayeshtejwani8055 <mobiledeveloper.jayesh@gmail.com>
2023-10-30 13:42:55 +0530

    Selection images losses position bug resolved

--- FMPhotoPicker/FMPhotoPicker/source/Data/FMPhotosDataSource.swift
+++ FMPhotoPicker/FMPhotoPicker/source/Data/FMPhotosDataSource.swift
@@ -12,40 +12,42 @@ import Photos
 class FMPhotosDataSource {
     public private(set) var photoAssets: [FMPhotoAsset]
     private var selectedPhotoIndexes: [Int]
-    
+
     init(photoAssets: [FMPhotoAsset]) {
-        self.photoAssets = photoAssets
+        self.photoAssets = photoAssets.filter({ aFMPhotoAsset in
+           return aFMPhotoAsset.asset != nil || aFMPhotoAsset.sourceImage != nil
+        })
         self.selectedPhotoIndexes = []
     }
-    
+
     public func setSeletedForPhoto(atIndex index: Int) {
         if self.selectedPhotoIndexes.firstIndex(where: { $0 == index }) == nil {
             self.selectedPhotoIndexes.append(index)
         }
     }
-    
+
     public func unsetSeclectedForPhoto(atIndex index: Int) {
         if let indexInSelectedIndex = self.selectedPhotoIndexes.firstIndex(where: { $0 == index }) {
             self.selectedPhotoIndexes.remove(at: indexInSelectedIndex)
         }
     }
-    
+
     public func selectedIndexOfPhoto(atIndex index: Int) -> Int? {
         return self.selectedPhotoIndexes.firstIndex(where: { $0 == index })
     }
-    
+
     public func numberOfSelectedPhoto() -> Int {
         return self.selectedPhotoIndexes.count
     }
-    
+
     public func mediaTypeForPhoto(atIndex index: Int) -> FMMediaType? {
         return self.photo(atIndex: index)?.mediaType
     }
-    
+
     public func countSelectedPhoto(byType: FMMediaType) -> Int {
         return self.getSelectedPhotos().filter { $0.mediaType == byType }.count
     }
-    
+
     public func affectedSelectedIndexs(changedIndex: Int) -> [Int] {
         return Array(self.selectedPhotoIndexes[changedIndex...])
     }
@@ -59,28 +61,28 @@ class FMPhotosDataSource {
         }
         return result
     }
-    
+
     public var numberOfPhotos: Int {
         return self.photoAssets.count
     }
-    
+
     public func photo(atIndex index: Int) -> FMPhotoAsset? {
         guard index < self.photoAssets.count, index >= 0 else { return nil }
         return self.photoAssets[index]
     }
-    
+
     public func index(ofPhoto photo: FMPhotoAsset) -> Int? {
         return self.photoAssets.firstIndex(where: { $0 === photo })
     }
-    
+
     public func contains(photo: FMPhotoAsset) -> Bool {
         return self.index(ofPhoto: photo) != nil
     }
-    
+
     public func delete(photo: FMPhotoAsset) {
         if let index = self.index(ofPhoto: photo) {
             self.photoAssets.remove(at: index)
         }
     }
-    
+
 }
